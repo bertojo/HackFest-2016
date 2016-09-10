45471fe2c53512a39c6ad6cf4e42824686bbaec2
bernardyip <bernard_yip40@hotmail.com>
2016-09-10 20:11:22 +0800

    Revert "state3"
    This reverts commit 57a0b75c14fdf06a7c97796eeca943f80e05e248.

--- src/State3.java
+++ src/State3.java
@@ -5,24 +5,14 @@ import java.util.Collections;
 //Randomly assign roles to players and tell them their allies
 public class State3 {
     // assign roles to players
-    public static void initialAssignment(Game game, AvalonBot bot){
+    public static void initialAssignment(Game game){
         ArrayList<Player> players = game.players;
         Collections.shuffle(players);
         String[] roles = Roles.getRoles(players.size());
         int i = 0;
         for(Player player : players){
             player.role = roles[i];
-            String[] allies = new String[4];
-            allies = Roles.getAlly(player.role, players.size());
-            StringBuilder sb = new StringBuilder();
-            sb.append(Roles.roleDescription(player.role) + "\nYour allies are : ");
-            for (String ally : allies) {
-                sb.append(ally + " ");
-            }
             i++;
-            
-            
-            bot.sendMessage(sb.toString(), player.id);
         }
     }
 
