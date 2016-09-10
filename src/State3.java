ab3b688a536652ecb52dc7fab771dd381e2a9ab8
bernardyip <bernard_yip40@hotmail.com>
2016-09-10 17:30:40 +0800

    Fix ArrayList

--- src/State3.java
+++ src/State3.java
@@ -1,12 +1,12 @@
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 
 
 //Randomly assign roles to players and tell them their allies
 public class State3 {
     // assign roles to players
     public static void initialAssignment(Game game){
-        LinkedList<Player> players = game.players;
+        ArrayList<Player> players = game.players;
         Collections.shuffle(players);
         String[] roles = Roles.getRoles(players.size());
         int i = 0;
