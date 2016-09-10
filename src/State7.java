7b62504ae2461acd23a2deb85eb404b6fef38de5
Chan Jing Hui Benedict <chanjhben@hotmail.com>
2016-09-10 16:21:14 +0800

    Added State7 (incomplete)

--- src/State7.java
+++ src/State7.java
@@ -1,4 +1,10 @@
-
+// Approve/Reject states
 public class State7 {
-
+    public static void run(String message) {
+        if (message.equals("Approve")) {
+            State8.run(); // incomplete
+        } else {
+            State4.run();
+        }
+    }
 }
