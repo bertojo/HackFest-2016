08bf88978ac8f295d65f36915c244b2f4d2bf677
bernardyip <bernard_yip40@hotmail.com>
2016-09-10 21:07:26 +0800

    Add creation of map object on state 3

--- src/State2.java
+++ src/State2.java
@@ -11,7 +11,7 @@ public class State2 {
 			bot.sendMessage("Connection Establish! Please wait for the other players to establish the connection", message.getChatId());
 			for (Player player : game.players) {
 				if (player.name.equals(message.getFrom().getFirstName())) {
-					if (player.id != 0) {
+					if (player.id == 0) {
 						player.id = message.getChatId();
 						bot.sendMessage("Established connection with " + player.name + " (" + (game.pmChannelCount+1) + "/" + game.players.size() + ")", game.gameId);
 						if (++game.pmChannelCount == game.players.size()) {
