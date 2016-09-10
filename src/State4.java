ab52d6329aa72a8f3e4d9040fd6596617cf8929a
Albert Yeoh <bertthings@gmail.com>
2016-09-10 19:32:53 +0800

    trying to test special keyboard

--- src/State4.java
+++ src/State4.java
@@ -2,7 +2,8 @@
 
 public class State4 {
         
-    public static void updateKing(Game game) {
+    public static void updateKing(AvalonBot bot ,Game game) {
         game.kingCounter++;
+        State5.kingChoosePlayers(bot, game);
     }
 }
--- src/State5.java
+++ src/State5.java
@@ -1,25 +1,44 @@
+import java.awt.List;
+import java.util.ArrayList;
+
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.replykeyboard.ForceReplyKeyboard;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardHide;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 
 public class State5 {
-    public static void run(Game game) {
+    public static void kingChoosePlayers(AvalonBot bot, Game game) {
         Player king = game.king;
-        sendMessage("The king shall now choose who will go on the quest.", king.chatId);
+        bot.sendMessage("The king shall now choose who will go on the quest.", game.gameId);
+        ReplyKeyboardMarkup specialKeyboard = getQuestChoosingKeyboard(bot, game);
+        SendMessage keyboardMessage = new SendMessage();
+        String gameID = new String("" + game.gameId + "");
+        keyboardMessage.setChatId(gameID);
+        keyboardMessage.enableMarkdown(true);
+        keyboardMessage.setReplyMarkup(specialKeyboard);
+        bot.sendMessage("Please choose " + game.map.missionPlayerCount[game.questNumber] + " players.", king.id);
+        bot.sendMessage(keyboardMessage);
     }
-    
-    public boolean sendMessage(String message, long id) {
-        SendMessage sendMessageRequest = new SendMessage();
-        //Who should get from the message the sender that sent it.
-        sendMessageRequest.setChatId(Long.toString(id)); 
-        sendMessageRequest.setText(message);
-        try {
-            //At the end, so some magic and send the message ;)
-            sendMessage(sendMessageRequest); 
-            return true;
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-            return false;
+
+    public static ReplyKeyboardMarkup getQuestChoosingKeyboard(AvalonBot bot, Game game) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboad(false);
+
+        ArrayList<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+        for (int i = 0; i < game.players.size(); i++) {
+            KeyboardRow keyboardRow = new KeyboardRow();
+            keyboardRow.add(new KeyboardButton(game.players.get(i).name));
+            keyboard.add(keyboardRow);
         }
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        return replyKeyboardMarkup;
     }
+
 }
