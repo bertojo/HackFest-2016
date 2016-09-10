e8a1e124830ec82c86156af5aa6621b61a4d2369
Hui Qi <xxstarsgalorexx@gmail.com>
2016-09-10 22:01:30 +0800

    edited

--- src/State11.java
+++ src/State11.java
@@ -15,10 +15,10 @@ public class State11 {
             }
             
             //assassin is found 
-            bot.sendMessage(game.players.get(indexOfAssassin) + " is the Assassin! Please choose someone to be Merlin and fuck him!", message.getChatId());
+            bot.sendMessage(game.players.get(indexOfAssassin) + " is the Assassin! Please choose someone to be Merlin and fuck him!", game.gameId);
             //need to insert keyboard here for the assassin to choose the character to be merlin 
         } else { 
-            bot.sendMessage("LOYAL SERVANTS OF ARTHUR. YOU HAD ONE JOB AND YOU FUCKED UP.", message.getChatId());
+            bot.sendMessage("LOYAL SERVANTS OF ARTHUR. YOU HAD ONE JOB AND YOU FUCKED UP.", game.gameId);
         }
     }
 
