6d2fa19fcf63afe9a312902e1a1c7b1985555ca9
bernardyip <bernard_yip40@hotmail.com>
2016-09-11 12:05:09 +0800

    Fixed State2 game null

--- src/State2.java
+++ src/State2.java
@@ -7,7 +7,7 @@ public class State2 {
 	public static void run(AvalonBot bot, Message message, TreeMap<Long, Game> games) {
 		//Tell users their role if they pm the bot the gameid for their role
 		Game game = games.get(Long.parseLong(message.getText().replace("/", ""))*-1);
-		if (game.state == 2) {
+		if (game != null && game.state == 2) {
 			bot.sendMessage("Connection Established! Please wait for the other players to establish the connection", message.getChatId());
 			for (Player player : game.players) {
 				if (player.name.equals(message.getFrom().getFirstName())) {
